fdca582098599b11d0087b53e63fbd86c2dd85e2
rnz4 <rnz.leto@gmail.com>
2023-08-19 17:37:51 -0300

    PokeCard firts styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,21 @@
 import { useEffect } from "react";
 import PokemonService from "./../services/pokemon.service";
+import { PokeCard } from "../components/poke-card";
+import styled from "styled-components";
+
+const Layout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #000000;
+  height: 100vh;
+  //background: radial-gradient(
+  //  circle,
+  //  rgba(21, 65, 75, 1) 9%,
+  //  rgba(9, 107, 121, 0.938813025210084) 47%,
+  //  rgba(74, 80, 75, 1) 100%
+  //);
+`;
 
 export default function Home() {
   const fetchPokemons = async () => {
@@ -11,8 +27,8 @@ export default function Home() {
     fetchPokemons();
   }, []);
   return (
-    <>
-      <p>hola mundo</p>
-    </>
+    <Layout>
+      <PokeCard></PokeCard>
+    </Layout>
   );
 }
